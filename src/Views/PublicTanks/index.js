fc51b22732af5fdd89e87fac552f0c993baa9535
Bailey <bailey.elise.dennis@gmail.com>
2020-12-05 00:06:55 -0600

    I lied, added tank cards, now pulling data from firebase. More styling work

--- src/Views/PublicTanks/index.js
+++ src/Views/PublicTanks/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-export default function PublicTanks({ user }) {
-  console.warn(user);
+export default function PublicTanks() {
   return (
     <div className='d-flex justify-content-center m5'>
       <h1>PublicTanks</h1>
